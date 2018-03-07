e9a1850046f664e7d6bbf3c8fc5d555aaf3bd001
adrien <adomoison@gmail.com>
2018-03-06 23:32:50 -0800

    Delete oauthmanagement package to merge it in oauth2-micro-service

--- oauthmanagement/repo/dbmodel/model.go
+++
@@ -1,8 +0,0 @@
-package dbmodel
-
-import oauthdbmodel "github.com/adriendomoison/gobootapi/oauth/repo/dbmodel"
-
-type Interface interface {
-	FindByAccessToken(token string) (user oauthdbmodel.Access, err error)
-}
-
--- oauthmanagement/service/model/model.go
+++
@@ -1,5 +0,0 @@
-package model
-
-type Interface interface {
-	GetResourceOwnerId(token string) (userId uint)
-}
